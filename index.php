cf6e0f08150df1255a87ddb9a3501eb1b742a01e
rrc_fall2023_grennie <grennie@academic.rrc.ca>
2024-11-03 18:36:47 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+/*******w******** 
+    
+    Name: Gregory Rennie
+    Date: November 3, 2024
+    Description: Project - Magic: The Gathering CMS Index PHP
+
+****************/
+
+    require('connect.php');
+
+    $success = filter_input(INPUT_GET,'success', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $deleted = filter_input(INPUT_GET,'deleted', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $updated = filter_input(INPUT_GET,'updated', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    $query = "SELECT * FROM Cards";
+    $statement = $db->prepare($query);
+    $statement->execute();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Welcome to the Magic: The Gathering Content Management System!</title>
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <?php if (null !==$success): ?>
+                <p>The Magic gathers!</p>
+            <?php endif ?>
+            <?php if (null !== $deleted): ?>
+                <p>The Magic disipates...</p>
+            <?php endif ?>
+            <?php if (null !== $updated): ?>
+                <p>The Magic is in flux!</p>
+            <?php endif ?>
+            <h1><a href="index.php">Magic: The Gathering Content Management System</a></h1>
+        </div>
+        <ul id="menu">
+            <li><a href="index.php" class="active">Home</a></li>
+            <li><a href="insert.php">Add Card</a></li>
+        </ul>
+        <?php while($row = $statement->fetch()): ?>
+            <h2><a href="show.php?id=<?= $row['CardID'] ?>"><?= $row['CardName'] ?></a></h2>
+            <p><small><a href="edit.php?id=<?= $row['CardID'] ?>">edit</a></small></p>
+            <p><?= $row['content'] ?></p>
+        <?php endwhile ?>
+    </div>
+</body>
+</html>
