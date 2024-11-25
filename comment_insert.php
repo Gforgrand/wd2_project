0695296accf02ed758cba7f099a42592c4f4a7cc
rrc_fall2023_grennie <grennie@academic.rrc.ca>
2024-11-25 02:40:19 -0600

    implemented comments on pages

---
+++ comment_insert.php
@@ -0,0 +1,37 @@
+<?php
+
+/*******w******** 
+    
+    Name: Gregory Rennie
+    Date: November 25, 2024
+    Description: Project - Magic: The Gathering CMS Insert Comment PHP
+
+****************/
+
+    session_start();
+
+    require('connect.php');
+
+    if (!isset($_SESSION['username'])) {
+        header("Location: index.php?accessdenied");
+        exit;
+    }
+
+    if($_POST && !empty(trim($_POST['content']))) {
+
+        $content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $cardid = filter_input(INPUT_POST, 'cardid', FILTER_SANITIZE_NUMBER_INT);
+
+        echo "Received Card ID: " . $cardid;
+        
+        $query = "INSERT INTO comments (cardid, username, content) VALUES (:cardid, :username, :content)";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':cardid', $cardid, PDO::PARAM_INT);
+        $statement->bindValue(':username', $_SESSION['username'], PDO::PARAM_STR);
+        $statement->bindValue(':content', $content, PDO::PARAM_STR);
+        $statement->execute();
+
+        header("Location: show.php?cardid=" . $cardid);
+        exit;
+    }
+?>
--- show.php
+++ show.php
@@ -8,9 +8,15 @@
 
 ****************/
 
+    session_start();
     require('connect.php');
 
     $cardid  = filter_input(INPUT_GET,'cardid', FILTER_SANITIZE_NUMBER_INT);
+
+    $comments_query = "SELECT * FROM comments WHERE cardid = :cardid ORDER BY commentid DESC";
+    $comments_statement = $db->prepare($comments_query);
+    $comments_statement->bindValue('cardid', $cardid, PDO::PARAM_INT);
+    $comments_statement->execute();
 
     $query = "SELECT c.*, t.cardtypename, m.colourname, s.cardsetname
               FROM cards c
@@ -22,9 +28,7 @@
               WHERE c.cardid = :cardid LIMIT 1";
     $statement = $db->prepare($query);
     $statement->bindValue('cardid', $cardid, PDO::PARAM_INT);
-
     $statement->execute();
-
     $row = $statement->fetch();
 
     $cardDetails = [
@@ -56,11 +60,29 @@
         <?php endif ?>
     </ul>
     <h2><?= $row['cardname'] ?></h2>
-    <p><small><a href="edit.php?cardid=<?= $row['cardid'] ?>">edit</a></small></p>
+    <?php if (isset($_SESSION['userlevel']) && $_SESSION['userlevel'] >= 20): ?>
+        <p><small><a href="edit.php?cardid=<?= $row['cardid'] ?>">edit</a></small></p>
+    <?php endif ?>
     <?php foreach ($cardDetails as $label => $value): ?>
         <?php if (!is_null($value) && !empty($value)): ?>
             <p><?= $label ?>: <?= $value ?></p>
         <?php endif ?>
-    <?php endforeach ?>        
+    <?php endforeach ?>
+    <?php if (isset($_SESSION['username'])): ?>
+        <form action=comment_insert.php method="post">
+            <input type="hidden" name="cardid" value="<?= $row['cardid'] ?>">
+            <fieldset>
+                <p>
+                    <label for="content">Comment</label>
+                    <textarea id="content" name="content"></textarea>
+                </p>
+                <input type="submit" value="Comment">
+            </fieldset>
+        </form>
+    <?php endif ?> 
+    <?php while($comment = $comments_statement->fetch()): ?>
+        <p><small>On <?= date("F d, Y, h:i a", strtotime($comment['timestamp'])); ?>, <?= $comment['username'] ?> said:</small></p>
+        <p><?= $comment['content'] ?></p>
+    <?php endwhile ?>
 </body>
 </html>
